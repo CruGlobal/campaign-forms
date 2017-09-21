80d246b595c320386c204dbfd52d8073530636b4
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-09-21 14:50:03 -0400

    Add monitors/lb endpoint

---
+++ app/controllers/monitors_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class MonitorsController < ApplicationController
+  protect_from_forgery with: :exception
+
+  layout nil
+  newrelic_ignore
+
+  def lb
+    render plain: File.read(Rails.public_path.join('lb.txt'))
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :forms, only: [:create]
+
+  get 'monitors/lb'
 end
