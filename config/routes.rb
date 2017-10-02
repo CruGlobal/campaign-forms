437bd44f6690ce3164dc8a5397a7bcf1158a09e9
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-10-02 11:28:14 -0400

    Add sidekiq web and protect with devise.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+require 'sidekiq/pro/web'
+
 Rails.application.routes.draw do
-  # devise_for :users, ActiveAdmin::Devise.config
   devise_for :users, class_name: 'User', controllers: { omniauth_callbacks: 'sessions' }
   ActiveAdmin.routes(self)
   devise_scope :user do
     get 'users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
+  authenticate :user, lambda { |user| user.has_access } do
+    mount Sidekiq::Web => '/sidekiq'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
