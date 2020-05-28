ca748e73f4960f3466583c8b1cd83faaf069b97d
Justin Sabelko <justin.sabelko@cru.org>
2020-05-28 14:39:20 -0500

    Rails 6: merge changes from railsdiff.org

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "action_controller/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+
 require_relative "../lib/log/logger"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,7 +22,7 @@ Bundler.require(*Rails.groups)
 module CampaignForms
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 require "sidekiq/pro/web"
 
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, class_name: "User", controllers: {omniauth_callbacks: "sessions"}
   ActiveAdmin.routes(self)
   devise_scope :user do
