c4cd6fd41ff1a9a2f4adf12ea02a8dc21c44ae8d
Justin Sabelko <justin.sabelko@cru.org>
2020-06-15 14:19:19 -0500

    Better login splash page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require("jquery")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,4 +2,13 @@ const { environment } = require('@rails/webpacker')
 const jquery = require('./plugins/jquery')
 
 environment.plugins.prepend('jquery', jquery)
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
+
 module.exports = environment
