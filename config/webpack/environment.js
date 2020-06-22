7dcc81df14b7f3a4f98daf109d546f86656c4773
Justin Sabelko <justin.sabelko@cru.org>
2020-06-22 14:11:42 -0500

    Don't add digest to campaign.js file

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
 const { environment } = require('@rails/webpacker')
 const jquery = require('./plugins/jquery')
 
+environment.config.set("output.filename", chunkData => {
+  // if the name of the pack is "widget" then exclude the chunk hash
+  if (chunkData.chunk.name === "campaign") return "campaign.js";
+  // otherwise fingerprint the asset
+  return "[name].[chunkhash].js";
+});
+
 environment.plugins.prepend('jquery', jquery)
 module.exports = environment
