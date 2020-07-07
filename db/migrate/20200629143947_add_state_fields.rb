e4b98b056899537c94c664e236f4256ac9218230
Mark Knutsen <mark.knutsen@cru.org>
2020-07-07 10:53:32 -0400

    Fix migration

--- db/migrate/20200629143947_add_state_fields.rb
+++ db/migrate/20200629143947_add_state_fields.rb
@@ -1,4 +1,6 @@
 class AddStateFields < ActiveRecord::Migration[5.2]
+  ITEMS_TO_SKIP = %w[AA AE AP AS DC GU MP PR UM VI]
+
   def up
     state = Field.find_or_create_by(name: "State",
                                     input: "select",
@@ -10,30 +12,28 @@ class AddStateFields < ActiveRecord::Migration[5.2]
     state.option_values.clear
     state.field_options.clear
 
-    items_to_skip = %w[AA AE AP AS UM VI]
-
     option = OptionValue.find_or_create_by(name: "AA", label: "Select a state")
     FieldOption.find_or_create_by(field: state, option_value: option, position: 0)
 
     states = ISO3166::Country.find_country_by_alpha3("USA").states.sort_by { |state| state[1].name }
     states.each_with_index do |item, index|
-      next if items_to_skip.include?(item[0])
+      next if ITEMS_TO_SKIP.include?(item[0])
       option_value = OptionValue.find_or_create_by(name: item[0], label: item[1].name)
       FieldOption.find_or_create_by(field: state, option_value: option_value, position: index + 1)
     end
   end
 
   def down
-    Field.find_by(name: "State").delete_all
-
     items_to_skip = %w[AA AE AP AS UM VI]
-
-    OptionValue.find_by(name: "AA", label: "Choose a state").delete_all
 
     states = ISO3166::Country.find_country_by_alpha3("USA").states
     states.each do |item|
-      next if items_to_skip.include?(item[0])
-      OptionValue.find_by(name: item[0], label: item[1].name).delete_all
+      next if ITEMS_TO_SKIP.include?(item[0])
+      OptionValue.find_by(name: item[0], label: item[1].name)&.destroy
     end
+
+    OptionValue.find_by(name: "AA", label: "Select a state")&.destroy
+
+    Field.find_by(name: "State")&.destroy
   end
 end
