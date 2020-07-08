5a8f7ff2923bd524bec8166bc9053bc441ae669c
Nicholas Rens <nicholas.rens@cru.org>
2020-07-08 09:34:28 -0500

    continue to validate with case_sensitive: false

--- app/models/option_value.rb
+++ app/models/option_value.rb
@@ -4,7 +4,7 @@ class OptionValue < ApplicationRecord
   has_many :field_options, dependent: :destroy
   has_many :fields, through: :field_options, dependent: :nullify
 
-  validates :label, uniqueness: { scope: :name }
+  validates :label, uniqueness: { scope: :name, case_sensitive: false }
 
   accepts_nested_attributes_for :field_options, allow_destroy: true
 end
