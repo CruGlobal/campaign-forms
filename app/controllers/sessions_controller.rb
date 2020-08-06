1d3713ee625c900ea957ffb6e74b992254dd2fee
Andrew Roth <andrewroth@gmail.com>
2020-08-06 12:04:57 -0400

    remember the id_token and pass it in the logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < Devise::OmniauthCallbacksController
   def oktaoauth
     @user = User.find_or_create_from_auth_hash(auth_hash)
     sign_in @user
+    session[:id_token] = request.env.dig("omniauth.auth", "extra", "id_token")
     redirect_to admin_forms_path
   end
 
