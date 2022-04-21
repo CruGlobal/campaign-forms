62fcbd87ffa6a88bd68fb49043787029eb8c9b1b
Mark Knutsen <mark.knutsen@cru.org>
2022-04-21 16:43:25 -0400

    Simple BriteVerify module

---
+++ app/lib/brite_verify.rb
@@ -0,0 +1,18 @@
+require "net/http"
+
+module BriteVerify
+  API_HOST = "bpi.briteverify.com"
+
+  def self.verify_email(email_address)
+    Net::HTTP.start(API_HOST, 443, use_ssl: true) do |http|
+      uri = URI("https://#{API_HOST}/api/v1/fullverify")
+      req = Net::HTTP::Post.new(uri)
+      req["Authorization"] = "ApiKey: #{ENV["BRITE_VERIFY_API_KEY"]}"
+      req.content_type = "application/json"
+      req.body = {email: email_address}.to_json
+      res = http.request(req)
+      return true if res.code != "200" # Verify by default if BriteVerify query doesn't work
+      JSON.parse(res.body).dig("email", "status") != "invalid" # Only an "invalid" response will fail
+    end
+  end
+end
