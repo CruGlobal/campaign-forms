a8eb8f79c3df7100da31e126eb3d7b5a11c34883
Mark Knutsen <mark.knutsen@cru.org>
2022-05-27 11:42:27 -0400

    Correction: role_address is a boolean, not an error_code

--- app/lib/brite_verify.rb
+++ app/lib/brite_verify.rb
@@ -6,7 +6,7 @@ module BriteVerify
 
   def self.valid_email?(email_address)
     # Policy: Only return false when BriteVerify says 'invalid'
-    # _and_ the secondary error_code is _not_ one of [mailbox_full_invalid role_address] - we like those.
+    # _and_ the secondary reason is _not_ one of: (mailbox_full_invalid, role_address) - we like those.
     # In all other cases, including protocol failure, return true.
     Net::HTTP.start(API_HOST, 443, use_ssl: true) do |http|
       req = Net::HTTP::Post.new(URI(FULLVERIFY))
@@ -14,10 +14,11 @@ module BriteVerify
       req.content_type = "application/json"
       req.body = {email: email_address}.to_json
       res = http.request(req)
-      return true if res.code != "200" # Verify by default if BriteVerify query doesn't work
-      res_json = JSON.parse(res.body) # Might error; rescued below to verify by default
+      return true if res.code != "200" # Validate by default if BriteVerify query doesn't work
+      res_json = JSON.parse(res.body) # Might error; rescued below to validate by default
       return true if res_json.dig("email", "status") != "invalid" # Empty or non-invalid response
-      %w[mailbox_full_invalid role_address].include?(res_json.dig("email", "error_code")) # Only accept these two invalid statuses
+      return true if res_json.dig("email", "role_address") == true # Invalid due to role_address; we validate these
+      return res_json.dig("email", "error_code") == "mailbox_full_invalid" # Also validate these; otherwise, invalid
     rescue JSON::ParserError
       return true
     end
