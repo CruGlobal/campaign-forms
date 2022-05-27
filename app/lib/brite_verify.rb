e181582e2d017429212272dce8bfad78f2fb17a1
Mark Knutsen <mark.knutsen@cru.org>
2022-05-27 10:01:50 -0400

    Alter BriteVerify policy slightly

--- app/lib/brite_verify.rb
+++ app/lib/brite_verify.rb
@@ -5,6 +5,9 @@ module BriteVerify
   FULLVERIFY = "https://#{API_HOST}/api/v1/fullverify"
 
   def self.valid_email?(email_address)
+    # Policy: Only return false when BriteVerify says 'invalid'
+    # _and_ the secondary error_code is _not_ one of [mailbox_full_invalid role_address] - we like those.
+    # In all other cases, including protocol failure, return true.
     Net::HTTP.start(API_HOST, 443, use_ssl: true) do |http|
       req = Net::HTTP::Post.new(URI(FULLVERIFY))
       req["Authorization"] = "ApiKey: #{ENV.fetch("BRITE_VERIFY_API_KEY")}"
@@ -12,7 +15,9 @@ module BriteVerify
       req.body = {email: email_address}.to_json
       res = http.request(req)
       return true if res.code != "200" # Verify by default if BriteVerify query doesn't work
-      JSON.parse(res.body).dig("email", "status") != "invalid" # Only an "invalid" response will fail
+      res_json = JSON.parse(res.body) # Might error; rescued below to verify by default
+      return true if res_json.dig("email", "status") != "invalid" # Empty or non-invalid response
+      %w[mailbox_full_invalid role_address].include?(res_json.dig("email", "error_code")) # Only accept these two invalid statuses
     rescue JSON::ParserError
       return true
     end
