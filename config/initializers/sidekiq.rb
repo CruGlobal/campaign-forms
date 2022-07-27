9b55de3b7100a45696a12b87bbe9a5e8d6b0755f
Mark Knutsen <mark.knutsen@cru.org>
2022-07-27 16:55:10 -0400

    More sidekiq-unique-jobs fixes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "redis"
+require "sidekiq-unique-jobs"
 
 redis_conf = YAML.safe_load(ERB.new(File.read(Rails.root.join("config", "redis.yml"))).result, [Symbol], [], true)["sidekiq"]
 
@@ -8,8 +9,19 @@ Redis.current = Redis.new(redis_conf)
 
 redis_settings = {url: Redis.current.id}
 
+SidekiqUniqueJobs.configure do |config|
+  # don't use SidekiqUniqueJobs in test env because it will cause head-scratching
+  # https://github.com/mhenrixon/sidekiq-unique-jobs#uniqueness
+  # https://github.com/mperham/sidekiq/wiki/Ent-Unique-Jobs#enable (not our gem but Sidekiq Enterprise suggested the same thing)
+  config.enabled = !Rails.env.test?
+end
+
 Sidekiq.configure_client do |config|
   config.redis = redis_settings
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
 end
 
 if Sidekiq::Client.method_defined? :reliable_push!
@@ -21,6 +33,16 @@ Sidekiq.configure_server do |config|
   config.reliable_scheduler!
   config.redis = redis_settings
   config.failures_default_mode = :exhausted
+
+  config.client_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Client
+  end
+
+  config.server_middleware do |chain|
+    chain.add SidekiqUniqueJobs::Middleware::Server
+  end
+
+  SidekiqUniqueJobs::Server.configure(config)
 end
 
 Sidekiq.default_job_options = {
