b396ce3b71805598a8064d5aa2143c9659b261f4
Justin Sabelko <justin.sabelko@cru.org>
2023-04-24 14:52:06 -0500

    Update Dockerfile

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ docker buildx build $DOCKER_ARGS \
     --build-arg PROJECT_NAME=$PROJECT_NAME \
     --build-arg SIDEKIQ_CREDS=$SIDEKIQ_CREDS \
     --build-arg SITE_URL=$SITE_URL \
+    --build-arg RUBY_VERSION=$(cat .ruby-version) \
     .
 rc=$?
 
