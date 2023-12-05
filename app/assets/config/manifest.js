344f49f7f9c5e236a80781fbb95d82a6f72842ac
Justin Sabelko <justin.sabelko@cru.org>
2023-12-05 09:53:16 -0600

    Fix manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_directory ../stylesheets .scss
-//= link_tree ../javascript .js
+//= link_directory ../javascript .js
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
