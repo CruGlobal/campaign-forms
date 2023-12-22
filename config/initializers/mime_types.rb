b7c8c121c88dcbc41b76db238219b4d1deb82cd0
Justin Sabelko <justin.sabelko@cru.org>
2023-12-22 14:17:03 -0600

    Appease standard

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,4 +3,4 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 
-Mime::Type.register "text/javascript", :mjs, %w( application/javascript application/x-javascript )
+Mime::Type.register "text/javascript", :mjs, %w[application/javascript application/x-javascript]
