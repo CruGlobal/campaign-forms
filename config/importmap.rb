b69ab80fd7ebd6e61467bc3c71f642306ee0fd72
Justin Sabelko <justin.sabelko@cru.org>
2024-03-28 14:57:31 -0500

    Fix javascript sources/mime type error

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,8 +5,10 @@ pin "application", preload: true
 
 # ActiveAdmin and dependencies
 pin "@activeadmin/activeadmin", to: "https://cdn.jsdelivr.net/npm/@activeadmin/activeadmin@2.13.1/app/assets/javascripts/active_admin/base.min.js"
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.0/dist/jquery.js"
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.0/dist/jquery.js"
 pin "jquery-ui", to: "https://cdn.jsdelivr.net/npm/jquery-ui@1.13.2/dist/jquery-ui.min.js"
 pin "jquery-ujs", to: "https://cdn.jsdelivr.net/npm/jquery-ujs@1.2.3/src/rails.min.js"
-pin "jquery-validation", to: "https://ga.jspm.io/npm:jquery-validation@1.19.5/dist/jquery.validate.js"
-pin "jquery-form", to: "https://ga.jspm.io/npm:jquery-form@4.3.0/dist/jquery.form.min.js"
+pin "jquery-validation", to: "https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.js"
+pin "jquery-form", to: "https://cdn.jsdelivr.net/npm/jquery-form@4.3.0/dist/jquery.form.min.js"
+
+pin "active_admin_custom", to: "custom/active_admin_custom.js"
